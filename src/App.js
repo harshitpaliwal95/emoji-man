25528ae56211d21904d178a0c19c71005a275283
Harshit paliwal <60725346+harshitpaliwal95@users.noreply.github.com>
2021-09-27 21:37:15 +0530

    add comments and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+// javascript objects
 const emojiDictionary = {
   "😊": "Smiling",
   "😎": "attitude",
@@ -21,11 +22,13 @@ const emojiDictionary = {
 };
 
 export default function App() {
+  // first assign
   let [emoji, setEmoji] = useState("");
   let [meaning, setMeaning] = useState("translation will appear here");
 
   const emojis = Object.keys(emojiDictionary);
 
+  // function for emoji scan
   function changeHandler(event) {
     const inputEmoji = event.target.value;
     setEmoji(inputEmoji);
@@ -36,12 +39,15 @@ export default function App() {
       setMeaning("failure to recognise");
     }
   }
+
+  // function for emoji list
   function emojiHandler(inputEmoji) {
     setEmoji(inputEmoji);
     setMeaning(emojiDictionary[inputEmoji]);
   }
 
   return (
+    // main section
     <div className="App">
       <h1>
         I am <span style={{ color: "blue" }}>Emoji man</span>{" "}
@@ -52,11 +58,14 @@ export default function App() {
       <input
         style={{ padding: "1rem", width: "80%" }}
         onChange={changeHandler}
+        placeholder="drop your emoji"
       />
       <div style={{ padding: "15px" }}>
         <h2>{emoji}</h2>
         <h3>{meaning}</h3>
       </div>
+
+      {/* showing all emoji list */}
       {emojis.map((emoji) => (
         <span
           onClick={() => emojiHandler(emoji)}
@@ -68,7 +77,30 @@ export default function App() {
         </span>
       ))}
 
-      <footer style={{ marginTop: "300px" }}>Made By Harshit Paliwal</footer>
+      {/* footer */}
+
+      <footer style={{ marginTop: "200px", padding: "12px" }}>
+        Made By Harshit Paliwal
+      </footer>
+
+      {/* links for stay connected */}
+
+      <div id="links">
+        <a href="https://twitter.com/harshit__hp" target="_blank">
+          <i class="fab fa-twitter fa-2x"></i>
+        </a>
+
+        <a href="https://github.com/harshitpaliwal95" target="_blank">
+          <i class="fab fa-github fa-2x"></i>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/harshit-paliwal-8302951b0/"
+          target="_blank"
+        >
+          <i class="fab fa-linkedin fa-2x"></i>
+        </a>
+      </div>
     </div>
   );
 }
